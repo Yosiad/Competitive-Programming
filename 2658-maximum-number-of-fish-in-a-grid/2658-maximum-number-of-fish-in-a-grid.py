ccb30b341c6b414bdc7763e9d682bdebf38cd128
Yosef Aweke <106531110+Yosiad@users.noreply.github.com>
2023-05-15 20:26:50 +0300

    Time: 692 ms (5.04%), Space: 16.6 MB (39.81%) - LeetHub

---
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxFish(self, grid: List[List[int]]) -> int:
+        def dfs(grid,row,col,seen):
+            if row<0 or row>=len(grid) or col<0 or col>=len(grid[0]) or grid[row][col]==0 or (row,col) in seen: return 0
+            seen.append((row,col))
+            return dfs(grid,row+1,col,seen) + dfs(grid,row-1,col,seen)+dfs(grid,row,col+1,seen)+dfs(grid,row,col-1,seen)+grid[row][col]
+        ans=0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col]!=0:
+                    ans=max(ans,dfs(grid,row,col,[]))
+        return ans 
