167fbde8849762448ac50d3b16f509c19c02cd64
Yosef Aweke <106531110+Yosiad@users.noreply.github.com>
2023-05-29 20:12:52 +0300

    Time: 87 ms (8.96%), Space: 16.9 MB (52.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        array=[]
+        array=''
         for i in s:
             if 64<ord(i)<91 or 96<ord(i)<123 :
-                array.append(i.lower()) 
+                array+=i.lower() 
             if  47<ord(i)<58:
-                array.append(int(i))
+                array+=str(int(i))
 
         return array==array[::-1]
         
