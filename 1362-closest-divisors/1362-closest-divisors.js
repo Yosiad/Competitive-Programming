e246f4d514346f26500754d4622d43ed48e70218
Yosef Aweke <106531110+Yosiad@users.noreply.github.com>
2024-12-25 15:26:31 +0300

    Time: 11 ms (20.00%), Space: 50 MB (22.86%) - LeetHub

---
+++ 1362-closest-divisors/1362-closest-divisors.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+var closestDivisors = function(num) {
+    let nums=[1, num]
+    for(let s=1;s<=Math.floor(Math.sqrt((num+3))); s++){
+        if(Number.isInteger((num+1)/s)){
+            if(Math.abs(nums[1]-nums[0])>((num+1)/s-s)) nums=[s,(num+1)/s];
+        }
+        if(Number.isInteger((num+2)/s)){
+            if(Math.abs(nums[1]-nums[0])>((num+2)/s-s)) nums=[s,(num+2)/s];
+        }
+    }
+    return nums;
+    
+};
